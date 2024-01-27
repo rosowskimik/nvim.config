efc478768799090314e1f7c84653b0c514b894e2
Mikołaj Rosowski <m.rosowski1@wp.pl>
2024-01-27 17:46:23 +0100

    update conflict plugin
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,9 +38,31 @@ return {
 
   { "lewis6991/gitsigns.nvim", config = true },
 
-  { "akinsho/git-conflict.nvim", version = "*", config = true },
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    config = function()
+      require("git-conflict").setup()
 
-  { "kylechui/nvim-surround", config = true },
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "GitConflictDetected",
+        callback = function()
+          vim.notify("Conflict detected in "..vim.fn.expand("<afile>"))
+          vim.keymap.set("n", "cww", function()
+            engage.conflict_buster()
+            create_buffer_local_mappings()
+          end)
+        end
+      })
+    end,
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = true,
+  },
 
   { "tpope/vim-sleuth" },
 

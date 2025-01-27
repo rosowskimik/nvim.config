4644571cbfac3164be3be020b5e7d3632cd5bdfc
Mikołaj Rosowski <m.rosowski1@wp.pl>
2025-01-27 19:46:52 +0100

    update vimtex config
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- lua/config/plugins/vimtex.lua
+++ lua/config/plugins/vimtex.lua
@@ -3,7 +3,12 @@ return {
     "lervag/vimtex",
     lazy = false,
     init = function()
-      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_method = "zathura_simple"
+      vim.g.vimtex_compiler_method = "tectonic"
     end,
+    keys = {
+      { "<localleader>c", "<cmd>VimtexCompile<CR>" },
+      { "<localleader>v", "<cmd>VimtexView<CR>" },
+    },
   },
 }
